810ad0ce6b2b98e01546a18b457054c951152026
Evan McCullough <e_mccullough92@yahoo.com>
2016-09-29 21:38:59 -0400

    updates

--- ajax.php
+++ ajax.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once 'lib/predis/autoload.php';
-require_once 'Spyc.class.php';
+require_once 'lib/Spyc.class.php';
 
 $config = Spyc::YAMLLoad('config.yaml');
 
